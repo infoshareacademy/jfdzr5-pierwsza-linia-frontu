87b84e9d81fd009b4d4c5affd3840ba351ce7f0a
Piotr Machnicki <piotr.machnicki@gmail.com>
2021-12-23 17:16:55 +0100

    Draft of main page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import './fontello.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="App-tiles">
+          <div className="App-tiles-item">
+            <p className="icon-paste"></p>
+            <h2>Lista zadań</h2>
+            <hr />
+            <p>Przeglądaj zadania do wykonania, dodaj nowe lub edytuj już istniejące.</p>
+          </div>
+          <div className="App-tiles-item">
+            <p className="icon-money"></p>
+            <h2>Budżet domowy</h2>
+            <hr />
+            <p>Zarządzaj domowym budżetem, wprowadź wpływy i wydatki, analizuj strukturę wydatków.</p>
+          </div>
+          <div className="App-tiles-item">
+            <p className="icon-bell-alt"></p>
+            <h2>Wydarzenia</h2>
+            <hr />
+            <p>Dodaj daty, rocznice i terminy, o których już nigdy nie zapomnisz.</p>
+          </div>
+          <div className="App-tiles-item">
+            <p className="icon-chart-pie"></p>
+            <h2>Dashboard</h2>
+            <hr />
+            <p>Sprawdź dane i zasoby aplikacji.</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
