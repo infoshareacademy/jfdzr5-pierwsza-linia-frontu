a6482a462f4ac2570a155f301a65f9a32bae88f3
Piotr Machnicki <piotr.machnicki@gmail.com>
2022-01-09 17:55:00 +0100

    clearing directories

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 import './App.css';
-import './fontello.css';
+import MainPage from './components/MainPage/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <div className="App-tiles">
-          <div className="App-tiles-item">
-            <p className="icon-paste"></p>
-            <h2>Lista zadań</h2>
-            <hr />
-            <p>Przeglądaj zadania do wykonania, dodaj nowe lub edytuj już istniejące.</p>
-          </div>
-          <div className="App-tiles-item">
-            <p className="icon-money"></p>
-            <h2>Budżet domowy</h2>
-            <hr />
-            <p>Zarządzaj domowym budżetem, wprowadź wpływy i wydatki, analizuj strukturę wydatków.</p>
-          </div>
-          <div className="App-tiles-item">
-            <p className="icon-bell-alt"></p>
-            <h2>Wydarzenia</h2>
-            <hr />
-            <p>Dodaj daty, rocznice i terminy, o których już nigdy nie zapomnisz.</p>
-          </div>
-          <div className="App-tiles-item">
-            <p className="icon-chart-pie"></p>
-            <h2>Dashboard</h2>
-            <hr />
-            <p>Sprawdź dane i zasoby aplikacji.</p>
-          </div>
-        </div>
-      </main>
-    </div>
+    <>
+      <MainPage />
+    </>
   );
 }
 
