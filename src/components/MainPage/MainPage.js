e0954f651860a63d78c2c2167e623b2dd497eae0
Piotr Machnicki <piotr.machnicki@gmail.com>
2022-01-09 18:52:16 +0100

    Main Page corrected, icons changed

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,28 +1,31 @@
-function mainPage(props) {
+import './MainPage.css';
+import { FaMoneyCheckAlt, FaPaste, FaBell, FaChartPie } from 'react-icons/fa';
+
+function MainPage() {
     return (
-        <div className="App">
+        <div className="mainpage">
             <main>
-                <div className="App-tiles">
-                    <div className="App-tiles-item">
-                        <p className="icon-paste"></p>
+                <div className="mainpage-tiles">
+                    <div className="mainpage-tiles-item">
+                        <FaPaste className='mainpage-icon' />
                         <h2>Lista zadań</h2>
                         <hr />
                         <p>Przeglądaj zadania do wykonania, dodaj nowe lub edytuj już istniejące.</p>
                     </div>
-                    <div className="App-tiles-item">
-                        <p className="icon-money"></p>
+                    <div className="mainpage-tiles-item">
+                        <FaMoneyCheckAlt className='mainpage-icon' />
                         <h2>Budżet domowy</h2>
                         <hr />
                         <p>Zarządzaj domowym budżetem, wprowadź wpływy i wydatki, analizuj strukturę wydatków.</p>
                     </div>
-                    <div className="App-tiles-item">
-                        <p className="icon-bell-alt"></p>
+                    <div className="mainpage-tiles-item">
+                        <FaBell className='mainpage-icon' />
                         <h2>Wydarzenia</h2>
                         <hr />
                         <p>Dodaj daty, rocznice i terminy, o których już nigdy nie zapomnisz.</p>
                     </div>
-                    <div className="App-tiles-item">
-                        <p className="icon-chart-pie"></p>
+                    <div className="mainpage-tiles-item">
+                        <FaChartPie className='mainpage-icon' />
                         <h2>Dashboard</h2>
                         <hr />
                         <p>Sprawdź dane i zasoby aplikacji.</p>
@@ -33,4 +36,4 @@ function mainPage(props) {
     );
 }
 
-export default mainPage;
+export default MainPage;
