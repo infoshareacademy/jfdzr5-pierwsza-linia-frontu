2be7a786b78b7bf557950e6003ccf6cffefa7cd9
Piotr Machnicki <piotr.machnicki@gmail.com>
2022-01-16 18:55:35 +0100

    corrected firebase

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   appId: "1:853847413126:web:a96507ca304cde83101c07"
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,7 +22,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
