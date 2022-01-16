82f3fb19254ad1521724d6645d5af36fd1aa89d3
Piotr Machnicki <piotr.machnicki@gmail.com>
2022-01-16 18:43:30 +0100

    added firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2a9vF2-RSVJU4CBsjm_1dYgAk4N3cPWI",
+  authDomain: "jfdzr5-bf21f.firebaseapp.com",
+  projectId: "jfdzr5-bf21f",
+  storageBucket: "jfdzr5-bf21f.appspot.com",
+  messagingSenderId: "853847413126",
+  appId: "1:853847413126:web:a96507ca304cde83101c07"
+};
+
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
