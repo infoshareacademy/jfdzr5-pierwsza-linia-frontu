c051e7b9b3b9800a58edf7633eed57a244bc3284
Dariusz Reczek <dariusz.reczek@gmail.com>
2022-01-21 20:31:44 +0100

    add working to do list with styles, add path in navigation to to do list, add to do list to content

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,21 +1,19 @@
-import { useState, useEffect } from "react";
-import AddTaskForm from "./AddTaskForm/AddTaskForm";
-import "./ToDoList.css";
+import { useState } from "react";
+import AddTaskForm from "./Tasks/AddTaskForm";
+import NewTask from "./Tasks/NewTask";
+
+import Container from "@mui/material/Container";
+import { Theme } from "../../common/theme";
+import { Typography } from "@mui/material";
+
 const ToDoList = () => {
   const [tasks, setTasks] = useState([
     { task: "zadanie 1", isCheckd: false, id: 1 },
     { task: "zadanie 2", isCheckd: true, id: 2 },
-    { task: "zadanie 3", isCheckd: false, id: 3 },
+    { task: "zadanie 3", isCheckd: true, id: 3 },
     { task: "zadanie 4", isCheckd: true, id: 4 },
   ]);
-  // const [tasks, setTasks] = useState([]);
-
-  const [save, setSave] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
   const [task, setTask] = useState("");
-  let [taskID, setTaskId] = useState("");
-  let [newValue2, setNewValue2] = useState("");
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -29,128 +27,26 @@ const ToDoList = () => {
     setTask("");
   };
 
-  const handleClickEdit = id => {
-    setIsEditing(true);
-    setSave(true);
-    setTaskId(id);
-    tasks.forEach(element => {
-      if (id === element.id) {
-        setNewValue2(element.task);
-      }
-    });
-    console.log("edit");
-    console.log(tasks);
-  };
-  const handleClickDelete = id => {
-    const newArray = tasks.filter(element => element.id !== id);
-    setTasks(newArray);
-  };
-
-  const handleClickSave = id => {
-    setSave(false);
-    const newArray = tasks.map(element => {
-      if (id === element.id) {
-        return { ...element, task: newValue2 };
-      }
-      return element;
-    });
-    setIsEditing(false);
-    // setTaskId(id);
-    // setTasks(newArray);
-    // return newArray, console.log(newArray);
-    console.log(newArray);
-    setTaskId("");
-    setTasks(newArray);
-  };
-  // const test = id => {
-  //   setTaskId(id);
-  // };
-  const handleIsChecked = e => {
-    console.log("klik");
-    const newArray = tasks.map(element => {
-      if (e.target.checked && taskID === element.id) {
-        return { ...element, isCheckd: true };
-      } else {
-        return { ...element, isCheckd: false };
-      }
-    });
-    // setIsEditing(false);
-    // setTaskId(id);
-    // setTasks(newArray);
-    // return newArray, console.log(newArray);
-    console.log(newArray);
-    // setTaskId("");
-    setTasks(newArray);
-    console.log(e.target.checked);
-    console.log(e);
-  };
   return (
-    <div className="to-do-list">
-      <h2 className="to-do-list-title">Lista zadań</h2>
-      <hr />
-
-      <AddTaskForm tasks={tasks} task={task} setTask={setTask} />
-      {/* <form className="form-to-add-task" onSubmit={handleSubmit}>
-        <div className="add-task-container">
-          <label>
-            <input
-              className="adding-task"
-              type="text"
-              placeholder="wpisz zadanie"
-              onChange={e => {
-                setTask(e.target.value);
-              }}
-              value={task}
-              required
-            />
-          </label>
-          <button className="to-do-buttons" type="submit">
-            dodaj
-          </button>
-        </div>
-      </form> */}
-      <div className="list-of-task-container">
-        {tasks.map(element => (
-          <div className="added-task" key={element.id}>
-            {element.id !== taskID && <p>{element.task}</p>}
-            {isEditing && element.id === taskID && (
-              <input
-                type="text"
-                value={newValue2}
-                onChange={e => {
-                  setNewValue2(e.target.value);
-                }}></input>
-            )}
-            <label>
-              <input
-                type="checkbox"
-                defaultChecked={element.isCheckd}
-                onChange={handleIsChecked}
-              />
-            </label>
-            {!save && (
-              <button
-                className="to-do-buttons"
-                onClick={() => handleClickEdit(element.id)}>
-                Edytuj
-              </button>
-            )}
-            {save && element.id === taskID && (
-              <button
-                className="to-do-buttons"
-                onClick={() => handleClickSave(element.id)}>
-                Zapisz
-              </button>
-            )}
-            <button
-              className="to-do-buttons"
-              onClick={() => handleClickDelete(element.id)}>
-              Usuń
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Container
+      maxWidth="sm"
+      sx={{
+        backgroundColor: Theme.palette.secondary.main,
+        margin: "0 auto",
+        marginTop: "10px",
+      }}>
+      <Container>
+        <Typography variant="h3" sx={{ textAlign: "center" }}>
+          Lista zadań
+        </Typography>
+        <AddTaskForm
+          task={task}
+          setTask={setTask}
+          handleSubmit={handleSubmit}
+        />
+        <NewTask tasks={tasks} setTasks={setTasks} />
+      </Container>
+    </Container>
   );
 };
 
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,29 +8,33 @@ import Button from "@mui/material/Button";
 import { Theme } from "../common/theme";
 
 const navItems = [
-    { label: 'Home', path: '/'},
-    { label: 'Zadania', path: '/tasks'},
-    { label: 'Budżet', path: '/budget'},
-    { label: 'Wydarzenia', path: '/events'},
-    { label: 'Dashboard', path: '/dashboard'},
-]
+  { label: "Home", path: "/" },
+  { label: "Zadania", path: "/tasks" },
+  { label: "Budżet", path: "/budget" },
+  { label: "Wydarzenia", path: "/events" },
+  { label: "Dashboard", path: "/dashboard" },
+];
 
 export const Navigation = () => {
-    return <AppBar position="static" theme={ Theme } color="secondary">
-        <Container maxWidth="xl">
-            <Toolbar disableGutters>
-                <IconButton>
-                    <Avatar alt="home" variant="square" src="https://cdn-icons.flaticon.com/png/512/2641/premium/2641242.png?token=exp=1642532667~hmac=3feed74840706f0edc33608bbd1c8e90"/>
-                </IconButton>
-                {
-                    navItems.map(item => (
-                        <Button key={item.label} sx={{my: 2, color: 'inherit'}} component={Link} to={item.path}>
-                            {item.label}
-                        </Button>
-                    ))
-                }
-                 <Button color="inherit">Login</Button>
-            </Toolbar>
-        </Container>
+  return (
+    <AppBar position="static" theme={Theme} color="secondary">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <IconButton>
+            <Avatar alt="home" variant="square" src="" />
+          </IconButton>
+          {navItems.map(item => (
+            <Button
+              key={item.label}
+              sx={{ my: 2, color: "inherit" }}
+              component={Link}
+              to={item.path}>
+              {item.label}
+            </Button>
+          ))}
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </Container>
     </AppBar>
-}
+  );
+};
