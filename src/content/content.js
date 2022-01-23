fcf1e1e75d56b1e8760fbb55fedfc36ac0fecbdb
Piotr Machnicki <piotr.machnicki@gmail.com>
2022-01-23 10:14:57 +0100

    added dashboard without real data

--- src/content/content.js
+++ src/content/content.js
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./home/home";
 import ToDoList from "../components/ToDoList/ToDoList";
 import Calendar from "../components/Calendar/Calendar";
+import Dashboard from "../components/Dashboard/Dashboard";
 
 export const Content = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/tasks" element={<ToDoList />} />
     <Route path="/calendar" element={<Calendar />} />
+    <Route path="/dashboard" element={<Dashboard />} />
   </Routes>
 );
