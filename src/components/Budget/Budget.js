01bdde0f1ef843c9e05428a74f6076bbaf13bacc
Liwia Dolinska <liwiamur@gmail.com>
2022-01-24 18:01:52 +0100

    added toggle layouts connected with buttons

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -2,10 +2,16 @@ import BudgetFormExpenses from "./BudgetComponents/BudgetFormExpenses";
 import BudgetFormIncomes from "./BudgetComponents/BudgetFormIncomes";
 import ExpensesList from "./BudgetComponents/ExpensesList";
 import IncomesList from "./BudgetComponents/IncomesList";
-import { MyButtons } from "./BudgetComponents/MyButtons";
 import { useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { PageWrapper } from "../../common/page-wrapper/page-wrapper";
+import { Button } from "@mui/material";
+
 
 export const Budget = () => {
+    const theme = useTheme();
+    console.log(theme)
+
     const [expenses, setExpenses] = useState([
 
     ]);
@@ -20,16 +26,47 @@ export const Budget = () => {
         setIncomes([...incomes, income])
     }
 
+    const [chosenMoneyOperations, setChosenMoneyOperations] = useState(
+        "expenses"
+    )
+
     return (
-        <>
+        <PageWrapper>
 
-            <MyButtons />
-            {/* <BudgetFormExpenses onSubmit={handleSubmit} /> */}
-            <BudgetFormExpenses onSubmit={handleExpenseSubmit} />
-            <BudgetFormIncomes onSubmit={handleIncomesSubmit} />
-            <ExpensesList expenses={expenses} />
-            <IncomesList incomes={incomes} />
+            <h1>Budżet domowy</h1>
+            <div>
+                <Button
+                    type="submit"
+                    variant="outlined"
+                    sx={{
+                        margin: "1rem",
+                        height: "3rem",
+                        color: theme.palette.primary,
+                        backgroundColor: theme.palette.secondary.contrastText,
+                        ":hover": { backgroundColor: theme.palette.primary.contrastText },
+                    }}
+                    onClick={() => setChosenMoneyOperations("expenses")}>
+                    Wydatki
+                </Button>
+                <button className={`button incomes ${chosenMoneyOperations === "incomes" ? "chosen" : ""}`} onClick={() => setChosenMoneyOperations("incomes")}>
+                    Przychody
+                </button>
+            </div>
+            {
+                chosenMoneyOperations === "expenses" ? (
+                    <>
+                        <BudgetFormExpenses onSubmit={handleExpenseSubmit} />
+                        <ExpensesList expenses={expenses} />
+                    </>
+                ) : (
+                    <>
+                        <BudgetFormIncomes onSubmit={handleIncomesSubmit} />
+                        <IncomesList incomes={incomes} />
+                    </>
+                )
+            }
 
-        </>
+
+        </PageWrapper>
     )
 }
