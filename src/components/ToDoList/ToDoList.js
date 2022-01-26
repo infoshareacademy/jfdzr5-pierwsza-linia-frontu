47ae1939ecc911b09d8ab23d272f4a9027e0b0e2
Dariusz Reczek <dariusz.reczek@gmail.com>
2022-01-26 18:54:27 +0100

    fetch data from firebase

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTaskForm from "./Tasks/AddTaskForm";
 import NewTask from "./Tasks/NewTask";
 
@@ -6,14 +6,52 @@ import Container from "@mui/material/Container";
 import { Theme } from "../../common/theme/theme";
 import { Typography } from "@mui/material";
 import { PageWrapper } from "../../common/page-wrapper/page-wrapper";
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  getDocs,
+} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const ToDoList = () => {
-  const [tasks, setTasks] = useState([
-    { task: "zadanie 1", isChecked: false, id: 1 },
-    { task: "zadanie 2", isChecked: true, id: 2 },
-    { task: "zadanie 3", isChecked: true, id: 3 },
-    { task: "zadanie 4", isChecked: true, id: 4 },
-  ]);
+  const db = getFirestore();
+  // const q = query(collection(db, "to-do-list"));
+  // console.log(q);
+
+  // const unsubscribe = onSnapshot(q, querySnapshot => {
+  //   const tasks = [];
+  //   querySnapshot.forEach(doc => {
+  //     tasks.push(doc.data().task);
+  //     tasks.push(doc.data().isChecked);
+  //     console.log(doc.data());
+  //   });
+
+  const colRef = collection(db, "to-do-list");
+  // getDocs(colRef).then(snapshot => {
+  //   console.log(snapshot);
+  // });
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async doc => {
+    doc = await getDocs(colRef);
+    let data = [];
+    doc.docs.forEach(element => {
+      data.push({ ...element.data(), id: element.id });
+    });
+    setTasks(data);
+  };
+  
+  const [tasks, setTasks] = useState([]);
+  console.log(tasks);
+  // const [tasks, setTasks] = useState([
+  //   { task: "zadanie 1", isChecked: false, id: 1 },
+  //   { task: "zadanie 2", isChecked: true, id: 2 },
+  //   { task: "zadanie 3", isChecked: true, id: 3 },
+  //   { task: "zadanie 4", isChecked: true, id: 4 },
+  // ]);
   const [task, setTask] = useState("");
 
   const handleSubmit = e => {
