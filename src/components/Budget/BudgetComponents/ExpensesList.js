2a9693c4d4ec1d098d7edf89f80c272f02d351cc
Liwia Dolinska <liwiamur@gmail.com>
2022-01-29 09:03:08 +0100

    styling

--- src/components/Budget/BudgetComponents/ExpensesList.js
+++ src/components/Budget/BudgetComponents/ExpensesList.js
@@ -1,10 +1,26 @@
 import React from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-import { Container, Input } from '@mui/material';
+import { Container } from '@mui/material';
+import styled from 'styled-components';
+import { Theme } from "../../../common/theme/theme";
 import { useState } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
+
+const NewExpenseContainer = styled.div`
+  display: flex;
+  min-height: 1rem;
+  margin: 10px;
+  padding: 10px;
+  background-color: grey;
+  color: ${Theme.palette.secondary.contrastText};
+  `;
 
 function ExpensesList(props) {
+
+
     function handleEdit() {
 
     }
@@ -15,22 +31,26 @@ function ExpensesList(props) {
     return (
         <div className="expenses-container">
             <ul className="expenses-list">
-                {
-                    props.expenses.map((expense) => {
-                        return <Container>
-                            (
-                            <li className="expenses" key={expense.id}>
-                                <span className="expenses-amount">{expense.amount} zł</span>
-                                <span className="expenses-category">{expense.category} </span>
-                                <span className="expenses-date">{expense.date}</span>
-                                <EditIcon onClick={() => handleEdit(expense.id)} />
-                                <DeleteIcon onClick={() => handleRemove(expense.id)} />
-                            </li>
+                <NewExpenseContainer >
+                    <List>
+                        {
+                            props.expenses.map((expense) => {
+                                return <Container>
+                                    (
+                                    <ListItem className="expenses" key={expense.id}>
+                                        <span className="expenses-amount">{expense.amount} zł    </span>
+                                        <span className="expenses-category">{expense.category}     </span>
+                                        <span className="expenses-date">{expense.date}</span>
+                                        <EditIcon onClick={() => handleEdit(expense.id)} />
+                                        <DeleteIcon onClick={() => handleRemove(expense.id)} />
+                                    </ListItem>
 
-                            )
-                        </Container>
-                    })
-                }
+                                    )
+                                </Container>
+                            })
+                        }
+                    </List>
+                </NewExpenseContainer>
 
             </ul>
         </div >
--- src/components/Budget/BudgetComponents/IncomesList.js
+++ src/components/Budget/BudgetComponents/IncomesList.js
@@ -1,25 +1,28 @@
 import React from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
 function IncomesList(props) {
     return (
         <div className="income-container">
             <ul className="income-list">
-                {
-                    props.incomes.map((income) => {
-                        return (
-                            <li className="incomes" key={income.id}>
-                                <span className="income-amount">{income.amount} zł</span>
-                                <span className="income-category">{income.category}</span>
-                                <span className="income-date">{income.date}</span>
-                                <EditIcon />
-                                <DeleteIcon />
-                            </li>
-                        )
-                    })
-                }
-
+                <List>
+                    {
+                        props.incomes.map((income) => {
+                            return (
+                                <ListItem className="incomes" key={income.id}>
+                                    <span className="income-amount">{income.amount} zł</span>
+                                    <span className="income-category">{income.category}</span>
+                                    <span className="income-date">{income.date}</span>
+                                    <EditIcon />
+                                    <DeleteIcon />
+                                </ListItem>
+                            )
+                        })
+                    }
+                </List>
             </ul>
         </div>
     )
