c2daecf7c94a557bf091e6314d703a29402c74b3
Dariusz Reczek <dariusz.reczek@gmail.com>
2022-02-04 18:40:08 +0100

    delete empty const

--- src/components/UserPanel/User/UserDetails.js
+++ src/components/UserPanel/User/UserDetails.js
@@ -2,7 +2,7 @@ import { Container, FormGroup, TextField, Typography } from "@mui/material";
 
 import styled from "styled-components";
 
-import { doc, deleteDoc, updateDoc, onSnapshot } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 import { SaveButton } from "../../ToDoList/buttons/SaveButton";
@@ -11,8 +11,7 @@ import { TextFieldReadOnly } from "../text-field/TextFieldReadOnly";
 import { TextFieldView } from "../text-field/TextFieldView";
 import { useContext } from "react";
 import { UserData } from "../../../UserData/UserData";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { UserContext } from "../../../userContext/UserContext";
+
 const DetailsContainer = styled.div`
   color: #fff;
   display: flex;
@@ -26,22 +25,10 @@ const DetailsContainer = styled.div`
 `;
 
 export const UserDetails = ({ userData, db }) => {
-  // const auth = getAuth();
-  // onAuthStateChanged(auth, user => {
-  //   if (user) {
-  //     // User is signed in, see docs for a list of available properties
-  //     // https://firebase.google.com/docs/reference/js/firebase.User
-  //     setUid(user.uid);
-  //     setUserEmail(user.email);
-  //     // ...
-  //   } else {
-  //     // User is signed out
-  //     // ...
-  //   }
-  // });
-
   const userDetailsData = useContext(UserData);
-
+  //get user uid and email from use context
+  const [uid, setUid] = useState("");
+  const [userEmail, setUserEmail] = useState("");
   useEffect(() => {
     console.log(userDetailsData);
     if (userDetailsData) {
@@ -52,12 +39,6 @@ export const UserDetails = ({ userData, db }) => {
     }
   });
 
-  const [uid, setUid] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-
-  const [save, setSave] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-  const [taskID, setTaskId] = useState("");
   const [takenValue, setTakenValue] = useState("");
   const [telephoneEdit, setTelephoneEdit] = useState(false);
   const [streetEdit, setStreetEdit] = useState(false);
@@ -65,37 +46,6 @@ export const UserDetails = ({ userData, db }) => {
   const [cityEdit, setCityEdit] = useState(false);
   const [postCodeEdit, setPostCodeEdit] = useState(false);
 
-  const handleClickEdit = e => {
-    handleClickCancel();
-    console.log(e);
-    userData.map(element => {
-      switch (e.target.value) {
-        case element.telephone:
-          setTakenValue(element.telephone);
-          setTelephoneEdit(true);
-          break;
-        case element.street:
-          setTakenValue(element.street);
-          setStreetEdit(true);
-          break;
-        case element.houseNumber:
-          setTakenValue(element.houseNumber);
-          setHouseNumberEdit(true);
-          break;
-        case element.city:
-          setTakenValue(element.city);
-          setCityEdit(true);
-          break;
-        case element.postcode:
-          setTakenValue(element.postcode);
-          setPostCodeEdit(true);
-          break;
-      }
-    });
-    if (!e.target.value) {
-      setTakenValue("d");
-    }
-  };
   const handleClickEditTelephone = e => {
     handleClickCancel();
     userData.map(element => {
@@ -153,8 +103,6 @@ export const UserDetails = ({ userData, db }) => {
   };
 
   const handleClickSave = async id => {
-    setIsEditing(true);
-    setTaskId("");
     const docRef = doc(db, "user-data", id);
     switch (true) {
       case telephoneEdit:
@@ -196,7 +144,6 @@ export const UserDetails = ({ userData, db }) => {
     setHouseNumberEdit(false);
     setCityEdit(false);
     setPostCodeEdit(false);
-    setTaskId("");
   };
   const handleOnClick = e => {
     e.preventDefault();
