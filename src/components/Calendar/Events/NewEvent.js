5a64c6c6145c369c8a46c800d32b6f2866add043
Dariusz Reczek <dariusz.reczek@gmail.com>
2022-02-15 18:52:30 +0100

    add UserID to task and show only taks connected with user

--- src/components/Calendar/Events/NewEvent.js
+++ src/components/Calendar/Events/NewEvent.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import styled from "styled-components";
 import { Theme } from "../../../common/theme/theme";
@@ -11,6 +11,9 @@ import { FormHelperText } from "@mui/material";
 import { Icon } from "@mui/material";
 import { OutlinedInput } from "@mui/material";
 import { Typography } from "@mui/material";
+
+import { useContext } from "react";
+import { UserContext } from "../../../userContext/UserContext";
 
 import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 
@@ -32,11 +35,21 @@ const NewEvent = ({ items, setItems, firestore }) => {
   const [takenDate, setTakenDate] = useState("");
   const [isAlerted, setIsAlerted] = useState();
 
-  const handleClickEdit = (id) => {
+  // uid from firebase -
+  const [uid, setUid] = useState("");
+  const { userUID } = useContext(UserContext);
+
+  useEffect(() => {
+    if (userUID) {
+      setUid(userUID);
+    }
+  });
+
+  const handleClickEdit = id => {
     setIsEditing(true);
     setSave(true);
     setEventId(id);
-    items.forEach((element) => {
+    items.forEach(element => {
       if (id === element.id) {
         setTakenName(element.name);
         setTakenDate(element.date);
@@ -44,7 +57,7 @@ const NewEvent = ({ items, setItems, firestore }) => {
       }
     });
   };
-  const handleClickDelete = (id) => {
+  const handleClickDelete = id => {
     const docRef = doc(firestore, "calendar", id);
     deleteDoc(docRef);
     setSave(false);
@@ -52,7 +65,7 @@ const NewEvent = ({ items, setItems, firestore }) => {
     setEventId("");
   };
 
-  const handleClickSave = async (id) => {
+  const handleClickSave = async id => {
     setSave(false);
     setIsEditing(false);
     setEventId("");
@@ -62,10 +75,12 @@ const NewEvent = ({ items, setItems, firestore }) => {
       name: takenName,
       date: takenDate,
       alert: isAlerted,
+      //add UserID to task
+      uid: uid,
     });
   };
 
-  const handleClickCancel = (id) => {
+  const handleClickCancel = id => {
     setIsEditing(false);
     setSave(false);
     setEventId("");
@@ -76,152 +91,149 @@ const NewEvent = ({ items, setItems, firestore }) => {
       sx={{
         backgroundColor: Theme.palette.primary.main,
         minWidth: "35vw",
-      }}
-    >
-      {items.map((element) => (
-        <NewEventContainer key={element.id}>
-          {element.id !== eventID && (
-            <>
-              <Typography
-                sx={{
-                  minWidth: "10vw",
-                  alignSelf: "center",
-                  padding: ".5rem",
-                }}
-              >
-                {element.name}
-              </Typography>
-              <Typography
-                sx={{
-                  marginLeft: "auto",
-                  alignSelf: "center",
-                  padding: ".5rem",
-                }}
-              >
-                {element.date}
-              </Typography>
-              <Checkbox
-                type="disabled"
-                sx={{
-                  [`&, &.${checkboxClasses.checked}`]: {
-                    color: Theme.palette.secondary.contrastText,
-                  },
-                  marginLeft: "1vw",
-                  alignSelf: "center",
-                  padding: ".5rem",
-                  cursor: "default",
-                  pointerEvents: "none",
-                }}
-                checked={element.alert ? true : false}
-              />
-            </>
-          )}
-          {isEditing && element.id === eventID && (
-            <>
-              <Box sx={{ paddingRight: ".5rem" }}>
-                <OutlinedInput
-                  autoFocus
-                  value={takenName}
-                  sx={{
-                    maxHeight: "2.5rem",
-                    padding: ".25rem",
-                    backgroundColor: Theme.palette.secondary.contrastText,
-                    ":hover": {
-                      backgroundColor: Theme.palette.primary.contrastText,
-                    },
-                  }}
-                  onChange={(e) => {
-                    setTakenName(e.target.value);
-                  }}
-                />
-              </Box>
-              <Box sx={{ padding: "0 1rem 0 .5rem" }}>
-                <OutlinedInput
-                  type="date"
-                  value={takenDate}
-                  sx={{
-                    maxHeight: "2.5rem",
-                    padding: ".25rem",
-                    backgroundColor: Theme.palette.secondary.contrastText,
-                    ":hover": {
-                      backgroundColor: Theme.palette.primary.contrastText,
-                    },
-                  }}
-                  onChange={(e) => {
-                    setTakenDate(e.target.value);
-                  }}
-                />
-              </Box>
-              <Box>
-                <Checkbox
-                  checked={isAlerted ? true : false}
-                  sx={{
-                    "& .MuiSvgIcon-root": { fontSize: "2.5rem" },
-                    [`&, &.${checkboxClasses.checked}`]: {
+      }}>
+      {items.map(
+        element =>
+        //check if task from firebase has UserID
+          element.uid === uid && (
+            <NewEventContainer key={element.id}>
+              {element.id !== eventID && (
+                <>
+                  <Typography
+                    sx={{
+                      minWidth: "10vw",
+                      alignSelf: "center",
+                      padding: ".5rem",
+                    }}>
+                    {element.name}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      marginLeft: "auto",
+                      alignSelf: "center",
+                      padding: ".5rem",
+                    }}>
+                    {element.date}
+                  </Typography>
+                  <Checkbox
+                    type="disabled"
+                    sx={{
+                      [`&, &.${checkboxClasses.checked}`]: {
+                        color: Theme.palette.secondary.contrastText,
+                      },
+                      marginLeft: "1vw",
+                      alignSelf: "center",
+                      padding: ".5rem",
+                      cursor: "default",
+                      pointerEvents: "none",
+                    }}
+                    checked={element.alert ? true : false}
+                  />
+                </>
+              )}
+              {isEditing && element.id === eventID && (
+                <>
+                  <Box sx={{ paddingRight: ".5rem" }}>
+                    <OutlinedInput
+                      autoFocus
+                      value={takenName}
+                      sx={{
+                        maxHeight: "2.5rem",
+                        padding: ".25rem",
+                        backgroundColor: Theme.palette.secondary.contrastText,
+                        ":hover": {
+                          backgroundColor: Theme.palette.primary.contrastText,
+                        },
+                      }}
+                      onChange={e => {
+                        setTakenName(e.target.value);
+                      }}
+                    />
+                  </Box>
+                  <Box sx={{ padding: "0 1rem 0 .5rem" }}>
+                    <OutlinedInput
+                      type="date"
+                      value={takenDate}
+                      sx={{
+                        maxHeight: "2.5rem",
+                        padding: ".25rem",
+                        backgroundColor: Theme.palette.secondary.contrastText,
+                        ":hover": {
+                          backgroundColor: Theme.palette.primary.contrastText,
+                        },
+                      }}
+                      onChange={e => {
+                        setTakenDate(e.target.value);
+                      }}
+                    />
+                  </Box>
+                  <Box>
+                    <Checkbox
+                      checked={isAlerted ? true : false}
+                      sx={{
+                        "& .MuiSvgIcon-root": { fontSize: "2.5rem" },
+                        [`&, &.${checkboxClasses.checked}`]: {
+                          color: Theme.palette.secondary.contrastText,
+                        },
+                        margin: 0,
+                        padding: 0,
+                        ":hover": {
+                          color: Theme.palette.primary.contrastText,
+                        },
+                      }}
+                      onChange={e => {
+                        setIsAlerted(e.target.checked ? true : false);
+                        console.log(isAlerted);
+                      }}
+                    />
+                  </Box>
+                </>
+              )}
+              {!save && (
+                <>
+                  <Button
+                    sx={{
+                      marginLeft: "6vw",
                       color: Theme.palette.secondary.contrastText,
-                    },
-                    margin: 0,
-                    padding: 0,
-                    ":hover": {
-                      color: Theme.palette.primary.contrastText,
-                    },
-                  }}
-                  onChange={(e) => {
-                    setIsAlerted(e.target.checked ? true : false);
-                    console.log(isAlerted);
-                  }}
-                />
-              </Box>
-            </>
-          )}
-          {!save && (
-            <>
-              <Button
-                sx={{
-                  marginLeft: "6vw",
-                  color: Theme.palette.secondary.contrastText,
-                  ":hover": { color: Theme.palette.primary.contrastText },
-                }}
-                onClick={() => handleClickEdit(element.id)}
-              >
-                <Icon>edit</Icon>
-              </Button>
-              <Button
-                sx={{
-                  color: Theme.palette.secondary.contrastText,
-                  ":hover": { color: Theme.palette.primary.contrastText },
-                }}
-                onClick={() => handleClickDelete(element.id)}
-              >
-                <Icon>delete</Icon>
-              </Button>
-            </>
-          )}
-          {save && element.id === eventID && (
-            <>
-              <Button
-                sx={{
-                  marginLeft: "auto",
-                  color: Theme.palette.secondary.contrastText,
-                  ":hover": { color: Theme.palette.primary.contrastText },
-                }}
-                onClick={() => handleClickCancel(element.id)}
-              >
-                <Icon>cancel</Icon>
-              </Button>
-              <Button
-                sx={{
-                  color: Theme.palette.secondary.contrastText,
-                  ":hover": { color: Theme.palette.primary.contrastText },
-                }}
-                onClick={() => handleClickSave(element.id)}
-              >
-                <Icon>save</Icon>
-              </Button>
-            </>
-          )}
-        </NewEventContainer>
-      ))}
+                      ":hover": { color: Theme.palette.primary.contrastText },
+                    }}
+                    onClick={() => handleClickEdit(element.id)}>
+                    <Icon>edit</Icon>
+                  </Button>
+                  <Button
+                    sx={{
+                      color: Theme.palette.secondary.contrastText,
+                      ":hover": { color: Theme.palette.primary.contrastText },
+                    }}
+                    onClick={() => handleClickDelete(element.id)}>
+                    <Icon>delete</Icon>
+                  </Button>
+                </>
+              )}
+              {save && element.id === eventID && (
+                <>
+                  <Button
+                    sx={{
+                      marginLeft: "auto",
+                      color: Theme.palette.secondary.contrastText,
+                      ":hover": { color: Theme.palette.primary.contrastText },
+                    }}
+                    onClick={() => handleClickCancel(element.id)}>
+                    <Icon>cancel</Icon>
+                  </Button>
+                  <Button
+                    sx={{
+                      color: Theme.palette.secondary.contrastText,
+                      ":hover": { color: Theme.palette.primary.contrastText },
+                    }}
+                    onClick={() => handleClickSave(element.id)}>
+                    <Icon>save</Icon>
+                  </Button>
+                </>
+              )}
+            </NewEventContainer>
+          )
+      )}
     </Box>
   );
 };
