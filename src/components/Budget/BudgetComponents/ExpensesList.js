3a2b6a82ce5d4c983e629db8e196cbb413529673
Liwia Dolinska <liwiamur@gmail.com>
2022-03-02 17:12:51 +0100

    added Box

--- src/components/Budget/BudgetComponents/ExpensesList.js
+++ src/components/Budget/BudgetComponents/ExpensesList.js
@@ -8,9 +8,11 @@ import ListItem from "@mui/material/ListItem";
 import { Button, Icon } from "@mui/material";
 import { OutlinedInput } from "@mui/material";
 import { FormHelperText } from "@mui/material";
+import { Box } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
-import { doc, updateDoc } from "firebase/firestore";
+// import { firestore } from "../../firebase";
+// import { doc, updateDoc } from "firebase/firestore";
 
 
 
@@ -36,7 +38,7 @@ function ExpensesList(props) {
 
   // const expensesColRef = collection(firestore, "budget-expenses");
 
-  const handleEditTask = (id) => {
+  const handleEditExpense = (id) => {
     setEditedTaskId(id)
     const editedTask = props.expenses.find((expense) => expense.id === id)
     setAmountInput(editedTask.amount)
@@ -64,132 +66,134 @@ function ExpensesList(props) {
   };
 
   return (
-    <div className="expenses-container">
-      <List>
-        {props.expenses.map(
-          expense =>
-            // return  jest niepotrzebny więc go usunąłem
-            //tutaj sprawdzam czy uid pobrane i przypisane do danego wydatku jest rowne uid danego uzytkownika
-            expense.uid === props.uid && (
-              <>
-                <NewExpenseContainer>
-                  <ListItem className="expenses" key={expense.id}>
-                    {
-                      expense.id === editedTaskId ? (<>
-                        <OutlinedInput
-                          required
-                          inputProps={{
-                            pattern: "[0-9]+(.|,)[0-9]{0,2}",
-                            title: "podaj liczbę z maks. 2 cyframi po przecinku ",
-                          }}
-                          placeholder="Podaj kwotę..."
-                          value={amountInput}
-                          onChange={handleAmountChange}
-                          sx={{
-                            width: "100%",
-                            height: "3rem",
-                            backgroundColor: Theme.palette.secondary.contrastText,
-                            ":hover": { backgroundColor: Theme.palette.primary.contrastText },
-                          }}></OutlinedInput>
-                        <FormHelperText
-                          sx={{
-                            margin: ".25rem",
-                            height: "1rem",
-                            color: Theme.palette.secondary.contrastText,
-                          }}>
-                          Kwota{" "}
-                        </FormHelperText>
+    <Box>
+      <div className="expenses-container">
+        <List>
+          {props.expenses.map(
+            expense =>
+              // return  jest niepotrzebny więc go usunąłem
+              //tutaj sprawdzam czy uid pobrane i przypisane do danego wydatku jest rowne uid danego uzytkownika
+              expense.uid === props.uid && (
+                <>
+                  <NewExpenseContainer>
+                    <ListItem className="expenses" key={expense.id}>
+                      {
+                        expense.id === editedTaskId ? (<>
+                          <OutlinedInput
+                            required
+                            inputProps={{
+                              pattern: "[0-9]+(.|,)[0-9]{0,2}",
+                              title: "podaj liczbę z maks. 2 cyframi po przecinku ",
+                            }}
+                            placeholder="Podaj kwotę..."
+                            value={amountInput}
+                            onChange={handleAmountChange}
+                            sx={{
+                              width: "100%",
+                              height: "3rem",
+                              backgroundColor: Theme.palette.secondary.contrastText,
+                              ":hover": { backgroundColor: Theme.palette.primary.contrastText },
+                            }}></OutlinedInput>
+                          <FormHelperText
+                            sx={{
+                              margin: "0.25rem",
+                              height: "1rem",
+                              color: Theme.palette.secondary.contrastText,
+                            }}>
+                            Kwota{" "}
+                          </FormHelperText>
 
-                        <Select
-                          required
-                          id="Category"
-                          value={categoryInput}
-                          onChange={handleCategoryChange}
-                          sx={{
-                            height: "3rem",
-                            width: "15rem",
-                            backgroundColor: Theme.palette.secondary.contrastText,
-                            ":hover": { backgroundColor: Theme.palette.primary.contrastText },
-                          }}>
-                          <MenuItem value="Jedzenie/Picie">Jedzenie/Napoje</MenuItem>
-                          <MenuItem value="Rachunki">Rachunki</MenuItem>
-                          <MenuItem value="Rozrywka">Rozrywka</MenuItem>
-                          <MenuItem value="Zakupy">Zakupy</MenuItem>
-                          <MenuItem value="Transport">Transport</MenuItem>
-                          <MenuItem value="Rodzina">Rodzina</MenuItem>
-                          <MenuItem value="Zwierzęta">Zwierzęta</MenuItem>
-                          <MenuItem value="Podróże">Podróże</MenuItem>
-                          <MenuItem value="Inne">Inne</MenuItem>
-                        </Select>
-                        <FormHelperText
-                          sx={{
-                            margin: ".25rem",
-                            height: "1rem",
-                            color: Theme.palette.secondary.contrastText,
-                          }}>
-                          Kategoria{" "}
-                        </FormHelperText>
+                          <Select
+                            required
+                            id="Category"
+                            value={categoryInput}
+                            onChange={handleCategoryChange}
+                            sx={{
+                              height: "3rem",
+                              width: "15rem",
+                              backgroundColor: Theme.palette.secondary.contrastText,
+                              ":hover": { backgroundColor: Theme.palette.primary.contrastText },
+                            }}>
+                            <MenuItem value="Jedzenie/Picie">Jedzenie/Napoje</MenuItem>
+                            <MenuItem value="Rachunki">Rachunki</MenuItem>
+                            <MenuItem value="Rozrywka">Rozrywka</MenuItem>
+                            <MenuItem value="Zakupy">Zakupy</MenuItem>
+                            <MenuItem value="Transport">Transport</MenuItem>
+                            <MenuItem value="Rodzina">Rodzina</MenuItem>
+                            <MenuItem value="Zwierzęta">Zwierzęta</MenuItem>
+                            <MenuItem value="Podróże">Podróże</MenuItem>
+                            <MenuItem value="Inne">Inne</MenuItem>
+                          </Select>
+                          <FormHelperText
+                            sx={{
+                              margin: ".25rem",
+                              height: "1rem",
+                              color: Theme.palette.secondary.contrastText,
+                            }}>
+                            Kategoria{" "}
+                          </FormHelperText>
 
-                        <OutlinedInput
-                          required
-                          type="date"
-                          value={dateInput}
-                          onChange={handleDateChange}
-                          sx={{
-                            width: "100%",
-                            height: "3rem",
-                            backgroundColor: Theme.palette.secondary.contrastText,
-                            ":hover": { backgroundColor: Theme.palette.primary.contrastText },
-                          }}
-                        />
-                        <FormHelperText
-                          sx={{
-                            margin: ".25rem",
-                            height: "1rem",
-                            color: Theme.palette.secondary.contrastText,
-                          }}>
-                          Data{" "}
-                        </FormHelperText>
-
-                        <Button>
-                          <Icon style={{ color: "white" }}>save</Icon>
-                        </Button>
-                        <Button>
-                          <Icon style={{ color: "white" }} onClick={() => handleClickCancel()}>cancel</Icon>
-                        </Button>
-
-
-                      </>) : (
-                        <>
-                          <ListItemElement>
-                            {expense.amount} zł{" "}
-                          </ListItemElement>
-                          <ListItemElement>
-                            {expense.category}
-                          </ListItemElement>
-                          <ListItemElement>
-                            {expense.date}
-                          </ListItemElement>
+                          <OutlinedInput
+                            required
+                            type="date"
+                            value={dateInput}
+                            onChange={handleDateChange}
+                            sx={{
+                              width: "100%",
+                              height: "3rem",
+                              backgroundColor: Theme.palette.secondary.contrastText,
+                              ":hover": { backgroundColor: Theme.palette.primary.contrastText },
+                            }}
+                          />
+                          <FormHelperText
+                            sx={{
+                              margin: ".25rem",
+                              height: "1rem",
+                              color: Theme.palette.secondary.contrastText,
+                            }}>
+                            Data{" "}
+                          </FormHelperText>
 
                           <Button>
-                            <DeleteIcon style={{ width: "4rem", color: "white" }} onClick={() => props.onDelete(expense.id)} />
+                            <Icon style={{ color: "white" }}>save</Icon>
                           </Button>
                           <Button>
-                            <EditIcon style={{ width: "4rem", color: "white" }} onClick={() => handleEditTask(expense.id)} />
+                            <Icon style={{ color: "white" }} onClick={() => handleClickCancel()}>cancel</Icon>
                           </Button>
-                        </>
-                      )
-
-                    }
 
 
-                  </ListItem>
-                </NewExpenseContainer>
-              </>
-            )
-        )}
-      </List>
-    </div>
+                        </>) : (
+                          <>
+                            <ListItemElement>
+                              {expense.amount} zł{" "}
+                            </ListItemElement>
+                            <ListItemElement>
+                              {expense.category}
+                            </ListItemElement>
+                            <ListItemElement>
+                              {expense.date}
+                            </ListItemElement>
+
+                            <Button>
+                              <DeleteIcon style={{ width: "4rem", color: "white" }} onClick={() => props.onDelete(expense.id)} />
+                            </Button>
+                            <Button>
+                              <EditIcon style={{ width: "4rem", color: "white" }} onClick={() => handleEditExpense(expense.id)} />
+                            </Button>
+                          </>
+                        )
+
+                      }
+
+
+                    </ListItem>
+                  </NewExpenseContainer>
+                </>
+              )
+          )}
+        </List>
+      </div>
+    </Box>
   );
 }
 
