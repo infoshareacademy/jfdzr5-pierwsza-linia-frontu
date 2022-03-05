2ce32d09c23333d9f380d5955214c718cd4248dd
Liwia Dolinska <liwiamur@gmail.com>
2022-03-05 17:32:51 +0100

    styling od deleteDialog

--- src/components/Budget/BudgetComponents/DeleteDialog.js
+++ src/components/Budget/BudgetComponents/DeleteDialog.js
@@ -3,39 +3,52 @@ import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogTitle from "@mui/material/DialogTitle";
-import { useTheme } from "@mui/material/styles";
+import { Theme } from "../../../common/theme/theme";
 
 export function DeleteDialog({ open, handleClose, handleDeleteTask }) {
-    const theme = useTheme();
-    const buttonProps = {
-        margin: "5px 5px 5px auto",
-        background: theme.palette.secondary.main,
-        color: theme.palette.secondary.contrastText,
-        border: `2px solid ${theme.palette.secondary.main}`,
-        borderRadius: "0px",
-        transition: "all",
-        transitionDuration: "0.3s",
-        ":hover": {
-            color: theme.palette.primary.main,
-            background: theme.palette.primary.contrastText,
-            border: `2px solid ${theme.palette.primary.contrastText}`,
-            borderRadius: "0",
-        },
-    }
+
     return (
         <Dialog
             open={open}
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description">
-            <DialogTitle id="alert-dialog-title">Czy usunąć zadanie?</DialogTitle>
+            <DialogTitle id="alert-dialog-title">Czy chcesz usunąć?</DialogTitle>
             <DialogActions>
                 <Button
-                    sx={buttonProps}
+                    sx={{
+                        margin: "5px 5px 5px auto",
+                        background: Theme.palette.secondary.main,
+                        color: Theme.palette.secondary.contrastText,
+                        border: `2px solid ${Theme.palette.secondary.main}`,
+                        borderRadius: "0px",
+                        transition: "all",
+                        transitionDuration: "0.3s",
+                        ":hover": {
+                            color: Theme.palette.primary.main,
+                            background: Theme.palette.primary.contrastText,
+                            border: `2px solid ${Theme.palette.primary.contrastText}`,
+                            borderRadius: "0",
+                        },
+                    }}
                     onClick={handleClose}>
                     Nie
                 </Button>
                 <Button
-                    sx={buttonProps}
+                    sx={{
+                        margin: "5px auto 5px 5px",
+                        background: Theme.palette.secondary.main,
+                        color: Theme.palette.secondary.contrastText,
+                        border: `2px solid ${Theme.palette.secondary.main}`,
+                        borderRadius: "0px",
+                        transition: "all",
+                        transitionDuration: "0.3s",
+                        ":hover": {
+                            color: Theme.palette.primary.main,
+                            background: Theme.palette.primary.contrastText,
+                            border: `2px solid ${Theme.palette.primary.contrastText}`,
+                            borderRadius: "0",
+                        },
+                    }}
                     onClick={handleDeleteTask}>
                     Tak
                 </Button>
