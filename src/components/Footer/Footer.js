e19996a4bcb5f8ff7e82f87305545d03a58b7589
Dariusz Reczek <dariusz.reczek@gmail.com>
2022-03-07 20:08:50 +0100

    add link to github repo of project to the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
-import { AppBar, Typography } from "@mui/material";
+import { AppBar, Button, Icon, Typography } from "@mui/material";
 import { Theme } from "../../common/theme/theme";
+import logo from "./git-hub-logo.svg";
 
 export const Footer = () => {
   return (
@@ -9,6 +10,19 @@ export const Footer = () => {
           variant="string"
           sx={{ padding: "10px", fontSize: "0.8rem" }}>
           Designed by: First Front Line ⓒ Wszelkie prawa zastrzeżone.
+          <Button
+            sx={{
+              color: Theme.palette.secondary.contrastText,
+              ":hover": { color: "Theme.palette.primary.contrastText " },
+            }}>
+            <Icon>
+              <a
+                href="https://github.com/infoshareacademy/jfdzr5-pierwsza-linia-frontu"
+                target="blank">
+                <img src={logo} alt="github-logo" />
+              </a>
+            </Icon>
+          </Button>
         </Typography>
       </AppBar>
     </>
